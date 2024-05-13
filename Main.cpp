af64eb19fb71a27fff48df148e2dd4c484ec6fd2
Sang Hyo <qscvbnmad@gmail.com>
2024-05-13 10:10:41 +0900

    init

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Number1 = 0;
+	int Number2 = 0;
+
+	cin >> Number1;
+	cin >> Number2;
+
+	cout << Number1 + Number2 << endl;
+	cout << Number1 - Number2 << endl;
+	cout << Number1 * Number2 << endl;
+	cout << Number1 / Number2 << endl;
+	
+	return 0;
+
+}
